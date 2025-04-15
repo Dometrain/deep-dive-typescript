788d3e589315c2f1c5fb3fe33a7066be15c89346
Cory House <housecor@gmail.com>
2025-04-15 11:56:05 -0500

    a

--- 3-utility-types/3.2-pick-and-omit/answers.ts
+++ 3-utility-types/3.2-pick-and-omit/answers.ts
@@ -1,7 +1,8 @@
 // Exercise: Testing knowledge of TypeScript's Pick and Omit utility types
 
 // Question 1:
-// Given the following interface, use the Pick utility type to create a new type that only includes the 'title' and 'completed' properties.
+// Given the following interface, use the Pick utility type to create a new
+// type that only includes the 'title' and 'completed' properties.
 
 interface Todo {
   id: number;
@@ -13,7 +14,8 @@ interface Todo {
 type TodoTitleAndCompleted = Pick<Todo, "title" | "completed">;
 
 // Question 2:
-// Given the following interface, use the Omit utility type to create a new type that excludes the 'id' and 'description' properties.
+// Given the following interface, use the Omit utility type to create a new
+// type that excludes the 'id' and 'description' properties.
 
 interface Task {
   id: number;

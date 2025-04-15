84a86f38e057f48892cde9cfae88aab84c393959
Cory House <housecor@gmail.com>
2025-04-15 11:15:53 -0500

    mv

--- 2-type-narrowing/2.10-assertion-function/answers.ts
+++ 2-type-narrowing/2.10-assertion-function/answers.ts
@@ -1,7 +1,8 @@
 // Exercise: Type Narrowing with Assertion Functions in TypeScript
 
 // Question 1:
-// Write an assertion function `assertIsNumber` that takes a parameter `value` of type `any` and throws an error if the value is not a number. Use this function to narrow the type of a variable.
+// Write an assertion function `assertIsNumber` that takes a parameter `value` of type `any`
+// and throws an error if the value is not a number. Use this function to narrow the type of a variable.
 
 function assertIsNumber(value: any): asserts value is number {
   if (typeof value !== "number") {
@@ -15,7 +16,8 @@ assertIsNumber(anotherValue);
 console.log(anotherValue.toFixed(2)); // This should work because `anotherValue` is now narrowed to `number`
 
 // Question 2:
-// Write an assertion function `assertIsArray` that takes a parameter `value` of type `any` and throws an error if the value is not an array. Use this function to narrow the type of a variable.
+// Write an assertion function `assertIsArray` that takes a parameter `value` of type `any`
+// and throws an error if the value is not an array. Use this function to narrow the type of a variable.
 
 function assertIsArray(value: any): asserts value is any[] {
   if (!Array.isArray(value)) {
--- 3-utility-types/3.1-intro/readme.md
+++ 3-utility-types/3.1-intro/readme.md
@@ -1,5 +1,5 @@
 # Utility Types Intro
 
-Sometimes you want to create new types from existing types. In these cases, consider using utility types.
+Sometimes you want to create a new type from an existing type. In these cases, consider using utility types.
 
 In "TypeScript: Getting Started", we used our first utility type, `Record`. Record creates a type for storing key value pairs, also known as a dictionary. But TypeScript provides several other utility types that help you manipulate types. They are generic types that take one or more type arguments and return a new type.

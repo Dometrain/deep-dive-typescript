7a5dfab3b834cb303087ab8401755db163700232
Cory House <housecor@gmail.com>
2025-04-17 16:53:45 -0500

    a

--- 5-mapped-types/5.3-keyof-operator/answers.ts
+++ 5-mapped-types/5.3-keyof-operator/answers.ts
@@ -1,21 +1,21 @@
 // Exercise: Testing knowledge of the `keyof` type operator in TypeScript
 
 // Question 1:
-// Given the following interface, use the `keyof` operator to create a type that represents the keys of the `Book` interface.
+// Given the following interface, use the `keyof` operator to create a union type
+// that represents the `Book` interface's keys.
 interface Book {
   title: string;
   author: string;
   publishedYear: number;
 }
 
-type BookKeys = keyof Book;
-// ^ "title" | "author" | "publishedYear"
+type BookKey = keyof Book; // "title" | "author" | "publishedYear"
 
 // Question 2:
-// Create a function `getProperty` that takes an object of type `Book` and a key of type `BookKeys`,
-// and returns the value of the specified key.
+// Create a function `getProperty` that takes an object of type `Book`
+// and a key of type `BookKey`, and returns the value of the specified key.
 
-function getProperty(obj: Book, key: BookKeys) {
+function getProperty(obj: Book, key: BookKey) {
   return obj[key];
 }
 
@@ -26,18 +26,4 @@ const book: Book = {
   publishedYear: 1993,
 };
 
-const name = getProperty(book, "title"); // "Steve McConnell"
-
-// Question 3: Extracting types from an interface
-// Question: Given the following interface, use keyof and the Extract utility type to create a union type called `StringProperties` that
-// only includes name and email.
-
-interface Person {
-  name: string;
-  age: number;
-  email: string;
-  isActive: boolean;
-}
-type StringProperties = Extract<keyof Person, "name" | "email">;
-
-type Answer2 = Extract<keyof Person, "name" | "email">; // 'name' | 'email'
+const name = getProperty(book, "author"); // "Steve McConnell"
--- 5-mapped-types/5.3-keyof-operator/exercise.ts
+++ 5-mapped-types/5.3-keyof-operator/exercise.ts
@@ -1,7 +1,8 @@
 // Exercise: Testing knowledge of the `keyof` type operator in TypeScript
 
 // Question 1:
-// Given the following interface, use the `keyof` operator to create a type that represents the keys of the `Book` interface.
+// Given the following interface, use the `keyof` operator to create a union type
+// that represents the `Book` interface's keys.
 interface Book {
   title: string;
   author: string;
@@ -9,8 +10,8 @@ interface Book {
 }
 
 // Question 2:
-// Create a function `getProperty` that takes an object of type `Book` and a key of type `BookKeys`,
-// and returns the value of the specified key.
+// Create a function `getProperty` that takes an object of type `Book`
+// and a key of type `BookKey`, and returns the value of the specified key.
 
 // Example usage:
 // const book: Book = {
@@ -19,15 +20,4 @@ interface Book {
 //   publishedYear: 1993,
 // };
 
-// const name = getProperty(book, "title"); // "Steve McConnell"
-
-// Exercise 3: Extracting types from an interface
-// Question: Given the following interface, use keyof and the Extract utility type
-// to create a union type called `StringProperties` that only includes name and email.
-
-interface Person {
-  name: string;
-  age: number;
-  email: string;
-  isActive: boolean;
-}
+// const name = getProperty(book, "author"); // "Steve McConnell"

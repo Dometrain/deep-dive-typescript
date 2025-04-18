1e4bfdb48597a75a3af11504316a1f140b4da038
Cory House <housecor@gmail.com>
2025-04-18 10:59:18 -0500

    a

--- 5-mapped-types/5.6-conditional-types/answers.ts
+++ 5-mapped-types/5.6-conditional-types/answers.ts
@@ -1,7 +1,8 @@
 // Exercise: Conditional Types in TypeScript
 
 // Question 1:
-// Create a type `IsString<T>` that evaluates to `true` if `T` is a string, and `false` otherwise.
+// Create a type `IsString<T>` that evaluates to `true`
+// if `T` is a string, and `false` otherwise.
 
 type IsString<T> = T extends string ? true : false;
 
@@ -23,3 +24,14 @@ type NonNullableType<T> = T extends null | undefined ? never : T;
 
 type Test5 = NonNullableType<string | null | undefined>; // string
 type Test6 = NonNullableType<number | undefined>; // number
+
+// Question 4:
+// Create a type `GetData` that takes an object type and returns
+// the type of the `data` property.
+
+type GetDataType<T> = T extends { data: any } ? T["data"] : never;
+
+// Usage
+type Result = GetDataType<{ data: "hi" }>; // "hi"
+type Result2 = GetDataType<{ data: { name: "hi" } }>; // { name: "hi" }
+type Result3 = GetDataType<"test">; // never
--- 5-mapped-types/5.6-conditional-types/exercise.ts
+++ 5-mapped-types/5.6-conditional-types/exercise.ts
@@ -8,3 +8,12 @@
 
 // Question 3:
 // Create a type `NonNullableType<T>` that removes `null` and `undefined` from `T`.
+
+// Question 4:
+// Create a type `GetData` that takes an object type and returns
+// the type of the `data` property.
+
+// Usage
+// type Result = GetDataType<{ data: "hi" }>; // "hi"
+// type Result2 = GetDataType<{ data: { name: "hi" } }>; // { name: "hi" }
+// type Result3 = GetDataType<"test">; // never

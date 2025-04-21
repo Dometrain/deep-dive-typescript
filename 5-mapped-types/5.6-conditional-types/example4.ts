c03620644322df828f6a28ef0a47bd01a9e23104
Cory House <housecor@gmail.com>
2025-04-21 10:44:18 -0500

    a

--- 5-mapped-types/5.6-conditional-types/example4.ts
+++ 5-mapped-types/5.6-conditional-types/example4.ts
@@ -1,5 +1,4 @@
-// Another distributive example:
-// When extending a union as a constraint, TS loops over each union member
+// Returning `never` in a conditional type omits the union member
 
 type Extract<T, U> = T extends U ? T : never; // Copy of TypeScript's Extract utility type - just here for reference
 
@@ -8,5 +7,5 @@ type Vehicle = "car" | "truck" | "bicycle";
 // So "T extends U" in TypeScript's `Extract` loops 3 times, once for each union entry in `Vehicle`:
 // 1. Is "car" assignable to "car" | "truck"? Yes, so keep it.
 // 2. Is "truck" assignable to "car" | "truck"? Yes, so keep it.
-// 4. Is "bicycle" assignable to "car" | "truck"? No, so discard it.
+// 3. Is "bicycle" assignable to "car" | "truck"? No, so discard it.
 type MotorizedVehicle = Extract<Vehicle, "car" | "truck">; // "car" | "truck"

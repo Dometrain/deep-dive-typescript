a2cd41686634b3b47789cfacd57844830c9d9a0b
Cory House <housecor@gmail.com>
2025-04-25 09:04:35 -0500

    add

---
+++ 10-ecosystem-and-devtools/3-testing-types/example1.ts
@@ -0,0 +1,8 @@
+// Testing types
+
+export type Equal<A, B> =
+  (<T>() => T extends A ? 1 : 2) extends <T>() => T extends B ? 1 : 2
+    ? true
+    : false;
+
+export type Expect<T extends true> = Equal<T, true>;

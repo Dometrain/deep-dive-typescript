efc86875ef407c8ffe80c433bdc44ffb07459c74
Cory House <housecor@gmail.com>
2025-04-29 12:38:44 -0500

    a

--- 4-generics/4.3-generic-interfaces/exercise.ts
+++ 4-generics/4.3-generic-interfaces/exercise.ts
@@ -1,6 +1,7 @@
 // Exercise 1: Generic Interface for a Key-Value Pair
 // Define a generic interface `KeyValuePair` that accepts two types: `K` for the key and `V` for the value.
-// Then, create a function `printKeyValuePair` that accepts an object of type `KeyValuePair` and logs the key and value.
+// Then, create a function `printKeyValuePair` that accepts an object of type `KeyValuePair`
+// and logs the key and value.
 
 // Test the function:
 // const stringNumberPair: KeyValuePair<string, number> = {

09835a8c9ef5ce9670e4cd4cdfc15dd65f45b6d4
Cory House <housecor@gmail.com>
2025-04-29 12:39:17 -0500

    a

--- 5-mapped-types/5.4-typeof/exercise.ts
+++ 5-mapped-types/5.4-typeof/exercise.ts
@@ -6,7 +6,7 @@ const productName = "Laptop";
 // Exercise 2: typeof with objects
 // Given the following object, do two things:
 // 1. Use the `typeof` operator to create a type alias for its structure.
-// 2. Create a union of the keys of the object's keys called `ProductKeys`.
+// 2. Create a union of the object's keys called `ProductKeys`.
 
 const product = {
   name: "Smartphone",
@@ -15,14 +15,16 @@ const product = {
 };
 
 // Exercise 3: typeof with functions
-// Given the following function and use the `typeof` operator to create a type alias for this function's type.
+// Given the following function and use the `typeof` operator to create
+// a type alias for this function's type.
 
 function calculateDiscount(price: number, discount: number): number {
   return price - price * discount;
 }
 
 // Exercise 4: typeof with arrays
-// Given the following array, use the `typeof` operator to create a type alias called `ProductCategoriesType`.
+// Given the following array, use the `typeof` operator to create
+// a type alias called `ProductCategoriesType`.
 
 const productCategories = ["Electronics", "Books", "Clothing"];
 

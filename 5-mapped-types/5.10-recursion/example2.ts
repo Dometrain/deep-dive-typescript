bc7b86c3588347984c1802c8eadbfcd9999a835a
Cory House <housecor@gmail.com>
2025-04-29 14:20:58 -0500

    change

--- 5-mapped-types/5.10-recursion/example2.ts
+++ 5-mapped-types/5.10-recursion/example2.ts
@@ -1,8 +1,18 @@
-// Recursively flatten an array
-type Flatten<T> = T extends Array<infer Item> ? Item : T;
+// Example: Recursive type that creates a union all keys from a nested object
 
-type FlattenDeep<T> = T extends Array<infer U> ? Flatten<U> : T;
+// Type with nested properties
+type Person = {
+  name: string;
+  address: {
+    city: string;
+    country: string;
+  };
+};
 
-// Usage
-type NestedArray = number[][][];
-type NumArray = FlattenDeep<NestedArray>; // number[]
+type AllKeys<T> = T extends object
+  ? {
+      [K in keyof T]: K | AllKeys<T[K]>; // Union of key and nested keys
+    } // Extract union of all values in the mapped type
+  : never;
+
+type PersonKeys = AllKeys<Person>; // "name" | "address" | "city" | "country"

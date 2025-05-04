7a96c99087f4583a4903cd648962df8e7f786478
Cory House <housecor@gmail.com>
2025-05-04 09:11:17 -0500

    a

--- 7-runtime-validation/7.7-web-app-example/src/url.ts
+++ 7-runtime-validation/7.7-web-app-example/src/url.ts
@@ -3,14 +3,22 @@ import * as z from "zod";
 
 const productIdSchema = z.coerce.number().positive(); // Tell Zod to coerce the productId in the URL to a numeric value
 
-const searchParams = new URLSearchParams(window.location.search);
-const productIdParam = searchParams.get("productId");
-if (productIdParam) {
-  const result = productIdSchema.safeParse(productIdParam);
-  if (result.success) {
-    const productId = result.data; // This will be a number
-    console.log("Valid URL parameter:", productId);
+export function setupProduct(productContainer: HTMLDivElement) {
+  const searchParams = new URLSearchParams(window.location.search);
+  const productIdParam = searchParams.get("product-id");
+  if (productIdParam) {
+    // Using safeParse so we can handle success and failure cases without a try/catch.
+    const result = productIdSchema.safeParse(productIdParam);
+
+    if (result.success) {
+      const productId = result.data; // number
+      productContainer.innerHTML = `product-id param: ${productId}`;
+    } else {
+      productContainer.innerHTML = `Invalid Product ID. It must be a number. See console for details.`;
+      console.error("Invalid Product ID:", result.error);
+    }
   } else {
-    console.error("Invalid URL parameter:", result.error);
+    productContainer.innerHTML = "No product-id param provided";
+    productContainer.classList.add("error");
   }
 }

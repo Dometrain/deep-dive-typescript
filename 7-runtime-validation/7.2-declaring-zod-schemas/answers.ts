9670dc4d0c436240e6e7ec606a9fa9abef0dd16a
Cory House <housecor@gmail.com>
2025-05-04 08:58:19 -0500

    a

--- 7-runtime-validation/7.2-declaring-zod-schemas/answers.ts
+++ 7-runtime-validation/7.2-declaring-zod-schemas/answers.ts
@@ -8,6 +8,7 @@ import * as z from "zod";
 // - age: a number that must be between 0 and 150
 // - email: a string that must be a valid email address
 // - isAdmin: a boolean
+// Then, use Zod's infer to derive an `Employee` type from the schema.
 
 const employeeSchema = z.object({
   id: z.string(),
@@ -27,9 +28,12 @@ const employeesSchema = z.array(employeeSchema);
 // - companyId: a string
 // - companyName: a string
 // - employees: an array of employees using the employeesSchema defined in question 2
+// Then, use Zod's infer to derive a `Company` type from the schema.
 
 const companySchema = z.object({
   companyId: z.string(),
   companyName: z.string(),
   employees: employeesSchema,
 });
+
+type Company = z.infer<typeof companySchema>;
--- 7-runtime-validation/7.2-declaring-zod-schemas/exercise.ts
+++ 7-runtime-validation/7.2-declaring-zod-schemas/exercise.ts
@@ -8,6 +8,7 @@ import * as z from "zod";
 // - age: a number that must be between 0 and 150
 // - email: a string that must be a valid email address
 // - isAdmin: a boolean
+// Then, use Zod's infer to derive an `Employee` type from the schema.
 
 // Question 2: Create a Zod schema for an array of employees using the employee schema defined above.
 
@@ -15,3 +16,4 @@ import * as z from "zod";
 // - companyId: a string
 // - companyName: a string
 // - employees: an array of employees using the employeesSchema defined in question 2
+// Then, use Zod's infer to derive a `Company` type from the schema.

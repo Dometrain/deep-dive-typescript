09f446e8288821c7daaa06fd829d332cc8b0d7df
Cory House <housecor@gmail.com>
2025-05-06 13:37:59 -0500

    add

--- 5-mapped-types/5.9-remapping-via-as/example1.ts
+++ 5-mapped-types/5.9-remapping-via-as/example1.ts
@@ -10,9 +10,34 @@ type RenameKeys<T> = {
   [K in keyof T as `new_${string & K}`]: T[K];
 };
 
-type OriginalType = {
+// Example usage
+type Original = {
   id: number;
   name: string;
-  price: number;
 };
-type RenamedType = RenameKeys<OriginalType>;
+
+type Renamed = RenameKeys<Original>;
+
+// Why `string &` type intersection?
+// ----------------------------------------------
+// Because TS doesn't know the key's type.
+
+// Example object with all potential valid key types
+const testSymbol = Symbol("test");
+const test1 = {
+  name: "test", // string key
+  1: "test", // number key (implicitly converted to string "1")
+  [testSymbol]: "test", // symbol key (not converted to string)
+};
+
+// Changing values for each key type
+test1.name = "new value"; // OK
+test1[1] = "new value"; // OK (implicitly converted to string "1")
+test1[testSymbol] = "new value"; // OK (symbol key)
+
+// Problem: Symbol isn't valid inside a template string:
+// const templateTestSymbol = `test${testSymbol}`; // Error
+
+// Solution: `string &` narrows the key type to string:
+type Key = string | number | symbol;
+type StringKey = string & Key;

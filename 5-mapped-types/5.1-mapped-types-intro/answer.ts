a6190c4e03b94aaeb99e875c29c60d103f3d14e9
Cory House <housecor@gmail.com>
2025-05-06 16:34:20 -0500

    a

---
+++ 5-mapped-types/5.1-mapped-types-intro/answer.ts
@@ -0,0 +1,12 @@
+// Exercise 1: Imagine your app allows the user to change their data if they have permissions.
+// Map over the `User` type to create a new type called `UserPermissions` with the same keys, but all boolean values.
+
+type User = {
+  email: string;
+  age: number;
+  role: string;
+};
+
+type UserPermissions = {
+  [P in keyof User]: boolean;
+};
---
+++ 5-mapped-types/5.1-mapped-types-intro/exercise.ts
@@ -0,0 +1,8 @@
+// Exercise 1: Imagine your app allows the user to change their data if they have permissions.
+// Map over the `User` type to create a new type called `UserPermissions` with the same keys, but all boolean values.
+
+type User = {
+  email: string;
+  age: number;
+  role: string;
+};

a9bf85f7f7fe7b5a3cbff02665c7a207ed835214
Cory House <housecor@gmail.com>
2025-05-07 09:05:02 -0500

    add

--- 8-decorators/8.5-decorating-methods/example1.ts
+++ 8-decorators/8.5-decorating-methods/example1.ts
@@ -1,4 +1,4 @@
-// Using this in TypeScript
+// Using `this` in TypeScript
 // The JS spec doesn't allow a parameter called `this`.
 // So, in TypeScript, you can declare the type for `this` in the function body as the first parameter.
 
@@ -6,17 +6,24 @@ interface Book {
   price: number;
   numberSold: number;
   getRevenue: (this: Book) => number;
+  getRevenueArrow: () => number;
 }
 
 const book: Book = {
   price: 65,
   numberSold: 1000,
-  getRevenue: function (this: Book) {
+  getRevenue: function () {
     return this.price * this.numberSold;
+  },
+  getRevenueArrow: () => {
+    return book.price * book.numberSold;
   },
 };
 
 console.log(book.getRevenue()); // works without a bind.
 
-const getTotalRevenue = book.getRevenue.bind(book); // Must bind the method to the object so it compiles
-getTotalRevenue(); // Error: this is undefined
+const getRevenue = book.getRevenue.bind(book); // Must bind the method to the object so it compiles
+getRevenue();
+
+const getRevenueArrow = book.getRevenueArrow; // No need to bind since arrow function inherits this from the enclosing scope
+getRevenueArrow(); // works without a bind.

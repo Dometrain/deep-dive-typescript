70742f71d3ab4807f29070692ee9af3e7eca07ba
Cory House <housecor@gmail.com>
2025-05-08 13:04:49 -0500

    a

--- 3-utility-types/3.9-other-utilities/example1.ts
+++ 3-utility-types/3.9-other-utilities/example1.ts
@@ -4,7 +4,7 @@
 // Example usage of NoInfer utility type
 function createCarModel<T extends string>(
   models: T[],
-  defaultModel?: NoInfer<T> // without NoInfer, TypeScript would infer the type of defaultModel as string
+  defaultModel?: NoInfer<T> // without NoInfer, TS infers as string
 ) {
   // ...
 }

2e511ba571290d00ce9a450b0585a42bc2c157d9
Cory House <housecor@gmail.com>
2025-05-11 10:41:28 -0500

    a

--- 9-misc-advanced-features-and-techniques/9.8-jsdocs-instead-of-ts/book.ts
+++
@@ -1,8 +0,0 @@
-export type Book = {
-  title: string;
-  pages: number;
-};
-
-export function describeBook(book: Book): string {
-  return `${book.title} has ${book.pages} pages`;
-}
--- 9-misc-advanced-features-and-techniques/9.8-jsdocs-instead-of-ts/example6.js
+++
@@ -1,9 +0,0 @@
-// Can use plain TS syntax too
-
-/** @typedef {{ id: number, name: string, age?: number }} User */
-/** @typedef {(data: string, index?: number) => boolean}  */
-
-let customer = /** @type {User} */ ({
-  id: 1,
-  name: "test",
-});

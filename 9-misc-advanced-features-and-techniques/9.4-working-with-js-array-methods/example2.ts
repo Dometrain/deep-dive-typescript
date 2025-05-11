d163355de621c175e91d9e1b70102c41a3905011
Cory House <housecor@gmail.com>
2025-05-11 14:40:11 -0500

    a

--- 9-misc-advanced-features-and-techniques/9.4-working-with-js-array-methods/example2.ts
+++ 9-misc-advanced-features-and-techniques/9.4-working-with-js-array-methods/example2.ts
@@ -2,5 +2,6 @@
 // Common mistake: Needlessly adding type annotations to the callback function
 
 const numbers = [1, 2, 3, 4, 5];
-const odd = numbers.filter((num) => num % 2 !== 0); // Note, no type annotation needed
+// Also note that you can omit params in the callback function (needlessly included here)
+const odd = numbers.filter((num, index, targetArray) => num % 2 !== 0); // Note, no type annotation needed
 console.log(odd); // [1, 3, 5]

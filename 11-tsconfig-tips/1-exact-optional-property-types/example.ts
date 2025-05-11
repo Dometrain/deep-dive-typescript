ef3806709704247c899dd36da84d494e40d0da14
Cory House <housecor@gmail.com>
2025-05-11 16:47:54 -0500

    a

--- 11-tsconfig-tips/1-exact-optional-property-types/example.ts
+++ 11-tsconfig-tips/1-exact-optional-property-types/example.ts
@@ -3,20 +3,7 @@
 interface Vehicle {
   manufactureDate?: Date;
 }
-interface Car extends Vehicle {
-  model: string;
-}
-class Garage {
-  vehicles: Vehicle[];
-  constructor(vehicles: Vehicle[]) {
-    this.vehicles = vehicles;
-  }
-}
-class CarGarage extends Garage {
-  // Does not emit JavaScript code,
-  // only ensures the types are correct
-  declare vehicles: Car[];
-  constructor(cars: Car[]) {
-    super(cars);
-  }
-}
+
+const vehicle: Vehicle = {
+  manufactureDate: undefined,
+};

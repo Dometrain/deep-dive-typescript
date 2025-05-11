834725858327c8bd9d3a9cd20a3c4565383c5170
Cory House <housecor@gmail.com>
2025-05-11 14:39:52 -0500

    a

--- 9-misc-advanced-features-and-techniques/9.9-named-tuples/example.ts
+++ 9-misc-advanced-features-and-techniques/9.9-named-tuples/example.ts
@@ -1,6 +1,13 @@
-// Named tuples are a way to give names to the elements of a tuple.
-// TODO: Supposed to have to specify name for all per docs, but omitting one seems to compile...
-type UserTuple = [name: string, age: number, boolean];
+// Named tuples label a tuple's elements.
+type Location = [lat: number, long: number];
 
-// And not seeing any benefits here:
-const user: UserTuple = ["Alice", 30, "true"];
+// Hover Location type to see the labels
+const nyc: Location = [40.7128, -74.006]; // New York City
+
+function logLocation(location: Location) {
+  const nycLat = nyc[0]; // Hover indexes to see labels.
+  console.log(`Lat: ${location[0]}, Long: ${location[1]}`);
+}
+
+// Unfortunately, no autocomplete hints for the labels though.
+logLocation([40.7128, -74.006]); // New York City

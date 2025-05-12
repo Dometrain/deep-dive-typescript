793c99354f8643db7535d82e563a44855fd00ecb
Cory House <housecor@gmail.com>
2025-05-12 11:31:42 -0500

    a

--- 11-tsconfig-tips/2-strictest/example1-unused-labels.ts
+++ 11-tsconfig-tips/2-strictest/example1-unused-labels.ts
@@ -1,8 +1,19 @@
-// Unused label example
+// Label example
 
+let str = "";
+
+loop1: for (let i = 0; i < 5; i++) {
+  if (i === 1) {
+    continue loop1; // skip to next iteration if i is 1
+  }
+  str = str + i;
+}
+
+console.log(str);
+
+// Unused label example
 function verifyAge(age: number) {
-  // Forgot 'return' statement
   if (age > 18) {
-    verified: true; // Unused label.
+    verified: true; // Unused label. Likely a typo.
   }
 }

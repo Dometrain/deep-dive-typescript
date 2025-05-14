a48fc08717db6a25b4ef59b677cc8197923a3cc1
Cory House <housecor@gmail.com>
2025-05-14 15:30:53 -0500

    a

--- 9-advanced-features-and-techniques/9.14-testing-types/example1.ts
+++ 9-advanced-features-and-techniques/9.14-testing-types/example1.ts
@@ -1,8 +1,14 @@
-// Q: Why would I write a test for this?
-// A: You wouldn't. This is a simple type that doesn't warrant testing.
+// Q: Why would I write a test for a type?
+// A: Because it's complex, and published via a package or library.
 
+// Not worth testing.
 type Vehicle = {
   make: string;
   model: string;
   year: number;
+};
+
+// Worth testing.
+type RecursivePartial<T> = {
+  [P in keyof T]?: T[P] extends object ? RecursivePartial<T[P]> : T[P];
 };

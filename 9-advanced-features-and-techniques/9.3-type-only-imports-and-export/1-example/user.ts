985cfaf6ee661e45d7dda019a4cac6d088afc1db
Cory House <housecor@gmail.com>
2025-05-14 15:34:25 -0500

    a

--- 9-advanced-features-and-techniques/9.3-type-only-imports-and-export/1-example/user.ts
+++ 9-advanced-features-and-techniques/9.3-type-only-imports-and-export/1-example/user.ts
@@ -1,8 +1,17 @@
-// import type { Address } from "./address"; // Favor type *outside* the braces if only importing types, since the import completely erases.
-// import { type Address } from "./address"; // This works too, but the import isn't fully erased when verbatimModuleSyntax is true.
-// import type { logAddress, Address } from "./address"; // Invalid. `hello` isn't a type
-import { logAddress, type Address } from "./address"; // Valid, and `logAddress` import is NOT erased since it's not a type.
-// import "./address"; // Import merely to preserve side effect.
+// Valid, and `logAddress` import is NOT erased since it's not a type.
+import { logAddress, type Address } from "./address";
+
+// Favor type *outside* the braces if only importing types, since the import completely erases.
+// import type { Address } from "./address";
+
+// This works too, but the import isn't fully erased when verbatimModuleSyntax is true.
+// import { type Address } from "./address";
+
+// Invalid. `logAddress` isn't a type
+// import type { logAddress, Address } from "./address";
+
+// Import merely to preserve side effect.
+// import "./address";
 
 type User = {
   id: number;

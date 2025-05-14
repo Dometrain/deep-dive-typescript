a76b8c65b56b2f0fcd7d79d2e0abab779203b44e
Cory House <housecor@gmail.com>
2025-05-14 15:47:13 -0500

    a

--- 9-advanced-features-and-techniques/9.5-typing-object-keys/example.ts
+++ 9-advanced-features-and-techniques/9.5-typing-object-keys/example.ts
@@ -1,6 +1,11 @@
 // Problem: object.keys returns string[]
 // We want the keys as a union of string literals
 
+// Solution: use a utility type
+// 1. Use type constraint to ensure T is an object
+// 2. Accept an object of type T
+// 3. The return type is an array of keys of T
+// 4. Cast the result of Object.keys to an array of T's keys
 const typedObjectKeys = <T extends object>(obj: T): (keyof T)[] => {
   return Object.keys(obj) as (keyof T)[];
 };

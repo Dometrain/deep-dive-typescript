f23255ca468f6ecb8cb244102b9d6d16584fc38f
Cory House <housecor@gmail.com>
2025-05-15 15:06:28 -0500

    a

--- 9-advanced-features-and-techniques/9.5-typing-object-keys/example.ts
+++ 9-advanced-features-and-techniques/9.5-typing-object-keys/example.ts
@@ -1,7 +1,7 @@
-// Problem: object.keys returns string[]
-// We want the keys as a union of string literals
+// Problem: object.keys returns string[]. Too "wide".
+// We want the keys as an array of string literals
 
-// Solution: use a utility type
+// Solution: This generic function.
 // 1. Use type constraint to ensure T is an object
 // 2. Accept an object of type T
 // 3. The return type is an array of keys of T

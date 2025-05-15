a96bb2c7a4bef750b83751cf5ae9ac4588dc21a7
Cory House <housecor@gmail.com>
2025-05-15 15:11:11 -0500

    a

--- 9-advanced-features-and-techniques/9.6-optional-key-vs-optional-value/example.ts
+++ 9-advanced-features-and-techniques/9.6-optional-key-vs-optional-value/example.ts
@@ -9,3 +9,12 @@ type UserWithOptionalValue = {
   id: number;
   email: string | null; // Can't forget to set this
 };
+
+const userWithOptionalKey: UserWithOptionalKey = {
+  id: 1, // Did I forget to set email?
+};
+
+const userWithOptionalValue: UserWithOptionalValue = {
+  id: 1,
+  email: null, // Can't forget to set this. Must explicitly choose null.
+};

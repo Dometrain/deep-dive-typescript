a7607c4c9d990cb34688f09b974d7e46151f0edc
Cory House <housecor@gmail.com>
2025-05-15 09:20:15 -0500

    a

---
+++ 9-advanced-features-and-techniques/9.14-testing-types/exercise.ts
@@ -0,0 +1,15 @@
+import { Equal, Expect } from "./example2";
+
+// Exercise 1: Use the Equal and Expect types in the example2.ts file to test the `RenameKeys` type.
+// This type should rename all keys in an object type to have a prefix of `new_`.
+// Use the `User` type below to test the `RenameKeys` type.
+
+type RenameKeys<T> = {
+  [K in keyof T as `new_${string & K}`]: T[K];
+};
+
+// Example usage
+type User = {
+  id: number;
+  name: string;
+};

436676acb56a84eced7d20bb4527eadbb49b4f7f
Cory House <housecor@gmail.com>
2025-05-15 14:53:51 -0500

    a

--- 9-advanced-features-and-techniques/9.4-named-tuples/answer.ts
+++ 9-advanced-features-and-techniques/9.4-named-tuples/answer.ts
@@ -1,3 +1,5 @@
 // Exercise 1:
 // Create a named tuple called `RGB` that represents the red, green, blue portions of a color.
-type RGB = [number: "red", number: "green", number: "blue"];
+type RGB = [red: number, green: number, blue: number];
+
+const color: RGB = [255, 0, 0];

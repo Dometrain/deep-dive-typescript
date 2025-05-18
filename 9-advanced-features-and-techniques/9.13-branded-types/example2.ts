a07d55be1905d8d2f11c6e6ae9e8ae81aef4cb54
Cory House <housecor@gmail.com>
2025-05-18 10:00:41 -0500

    a

--- 9-advanced-features-and-techniques/9.13-branded-types/example2.ts
+++ 9-advanced-features-and-techniques/9.13-branded-types/example2.ts
@@ -1,7 +1,7 @@
 // Risk: Structural types mean you may accidentally pass the wrong type
 type Car = { make: string };
-type Truck = { make: string };
+type Truck = { make: string; bedLength: number };
 
 const serviceCar = (car: Car) => {};
-const truck: Truck = { make: "Ford" };
+const truck: Truck = { make: "Ford", bedLength: 5 };
 serviceCar(truck); // Works. Ideally would get a type error.

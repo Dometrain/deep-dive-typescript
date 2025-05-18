c86f9ee24a19cc426c1cda2ff3b13d7bbef93d4c
Cory House <housecor@gmail.com>
2025-05-18 10:01:33 -0500

    a

--- 9-advanced-features-and-techniques/9.5-typing-object-keys/example.ts
+++ 9-advanced-features-and-techniques/9.5-typing-object-keys/example.ts
@@ -1,7 +1,20 @@
 // Problem: object.keys returns string[]. Too "wide".
 // We want the keys as an array of string literals
 
-// Solution: This generic function.
+const user = {
+  name: "Alice",
+  age: 30,
+  isStudent: false,
+};
+
+const wideKeys = Object.keys(user); // string[], even if I mark the object as const
+
+// Two ways to narrow the keys
+
+// Option 1. Type assertion
+const narrowKeys1 = Object.keys(user) as (keyof typeof user)[];
+
+// Option 2. Generic function.
 // 1. Use type constraint to ensure T is an object
 // 2. Accept an object of type T
 // 3. The return type is an array of keys of T
@@ -9,12 +22,4 @@
 const typedObjectKeys = <T extends object>(obj: T): (keyof T)[] => {
   return Object.keys(obj) as (keyof T)[];
 };
-
-const myObject = {
-  name: "Alice",
-  age: 30,
-  isStudent: false,
-};
-
-const wideKeys = Object.keys(myObject); // string[]
-const narrowKeys = typedObjectKeys(myObject); // ("name" | "age" | "isStudent")[]
+const narrowKeys2 = typedObjectKeys(user); // ("name" | "age" | "isStudent")[]

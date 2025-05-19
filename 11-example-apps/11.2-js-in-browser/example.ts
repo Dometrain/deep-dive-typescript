20e63c6bfe707ad513cfaad9026567b8bd92d26d
Cory House <housecor@gmail.com>
2025-05-19 10:14:43 -0500

    a

--- 11-example-apps/11.2-js-in-browser/example.ts
+++ 11-example-apps/11.2-js-in-browser/example.ts
@@ -1,19 +1,26 @@
-// TS doesn't know what the query will resolve to, so must use type assertion
-// to tell it that the element will be an HTMLButtonElement
+// TS doesn't know what the query will resolve to, so use type assertion
 const button = document.getElementById("change-text") as HTMLButtonElement;
 
-// Or, can use generic type parameter to specify the type
+// Or, use querySelector with a generic type parameter
 const button1 = document.querySelector<HTMLButtonElement>("#change-text");
 
-// Or, if we query by DOM element name, then TypeScript infers the type
-const button2 = document.querySelector("button");
+// or, for safety, narrow the type using a type guard;
+const button2 = document.getElementById("change-text");
+if (button2 instanceof HTMLButtonElement) {
+  // Now TS knows button3 is an HTMLButtonElement
+  button2.addEventListener("click", () => {
+    const textElement = document.getElementById("text-container");
+    if (textElement) {
+      textElement.innerHTML = "Changed text";
+    }
+  });
+} else {
+  // Optionally, handle the error case
+  console.error("Element is not a button");
+}
 
-button.addEventListener("click", () => {
-  const textElement = document.getElementById("text-container");
-  if (textElement) {
-    textElement.innerHTML = "Changed text";
-  }
-});
+// Or, if you query by DOM element name, TS infers the type, but may be null
+const button3 = document.querySelector("button");
 
 // Also get a precise type based on the element created
 const input = document.createElement("input");
@@ -24,7 +31,7 @@ input.value = "Test input";
 // Use optional chaining to avoid errors if null
 document.querySelector("body")?.appendChild(input);
 
-// Or, can use ! to assert that the element is not null
+// Or, can use a non-null assertion
 document.querySelector("body")!.appendChild(input);
 
 const appDiv = document.querySelector<HTMLDivElement>("#app")!;

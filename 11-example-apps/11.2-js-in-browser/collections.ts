fdb2250220688899f08afae3ddbacb4ca1895fdc
Cory House <housecor@gmail.com>
2025-05-19 10:14:49 -0500

    a

--- 11-example-apps/11.2-js-in-browser/collections.ts
+++ 11-example-apps/11.2-js-in-browser/collections.ts
@@ -5,5 +5,9 @@ console.log(ul.children); // HTMLCollection(2) [li, li] (only <li> elements)
 console.log(ul.childNodes); // NodeList(5) (newline, text, <li>, text, <li>)
 
 // Can also use querySelector or querySelectorAll to get elements, but return different types
-const first = document.querySelector("li"); // one HTMLLIElement
+const first = document.querySelector("li"); // HTMLLIElement | null
 const all = document.querySelectorAll("li"); // NodeListOf<HTMLLIElement>, so can iterate over it
+
+all.forEach((li) => {
+  console.log(li.innerHTML); // <li>Item 1</li>, <li>Item 2</li>
+});
